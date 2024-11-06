b79d8349b52c31859df581a72658f82d6430e0c9
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-11-06 08:03:28 +0700

    default parameter, destructuring

--- lession2/main.js
+++ lession2/main.js
@@ -33,4 +33,59 @@ var sayHello4 = name => `Hello ${name}`;
 
 // console.log(sayHello4('chinhpd5'));
 
+// Default parameter : giá trị mặc định cho tham số
+var sum = (a, b=0) => a+b ;
+
+// console.log(sum(5)); // NaN // 0
+
+// Destructuring
+
+var myArray = [1,2,3,4];
+
+// console.log(myArray[1]); //2
+
+var [a,b,,d] = myArray;
+// console.log(a); // 1
+// console.log(b); // 2
+// // console.log(c); // undefined
+// console.log(d); // 4
+
+var myObject = {
+    name: "chinhpd5",
+    age: 20,
+    child: {
+        name : "chinhpd6"
+    }
+}
+
+// console.log(myObject.name);
+// console.log(myObject.age);
+
+var {name,age, child:{name : childName}} = myObject;
+// console.log(name);
+// console.log(age);
+// console.log(childName);
+
+function showInfo (data){
+    console.log(data.name);
+    console.log(data.age);
+    console.log(data.child.name);
+}
+
+// showInfo(myObject)
+
+function showInfo2 ({name,age, child:{name : childName}}){
+    console.log(name);
+    console.log(age);
+    console.log(childName);
+}
+
+showInfo2(myObject)
+
+
+
+
+
+
+
 
