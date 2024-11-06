7551f17c958e68059fbd3d1f007bd800ab05d46e
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-11-06 20:12:51 +0700

    spread, nullish, optional chaining

--- lession2/main.js
+++ lession2/main.js
@@ -93,8 +93,103 @@ sum = (result,...rest)=>{
     return result;
 }   
 
-console.log(sum(1,2,3,4,5));
-console.log(sum(1,2,3,4,5,6,7,8,9));
+// console.log(sum(1,2,3,4,5));
+// console.log(sum(1,2,3,4,5,6,7,8,9));
+
+//spread ... : phân rã từ cái cũ
+
+var arr1 = [1,2];
+var arr2 = [3,4];
+
+// console.log(arr1.concat(arr2));
+
+var newArr = [...arr1, ...arr2];
+// console.log(newArr);
+
+var obj1 = {
+    name1: "chinhpd5",
+    age: 20
+}
+
+var obj2 ={
+    name: "chinhpd6"
+}
+
+var newObj = {...obj1, ...obj2};
+// console.log(newObj);
+
+var a = 1; //number - biến tham trị
+var b = a; // 1
+a = 2; // 2
+// console.log(b); // 1
+
+var a = {value: 1}; // object - biến tham chiếu
+var b = a; // gán vị trí ô nhớ của a cho b <=> a,b cùng ô nhớ
+a.value = 2; // khi a thay đổi thì b cũng thay đổi theo
+// console.log(b.value); // 2
+
+// Biến tham trị và biến tham chiếu JS
+
+// Biến tham trị
+// khi copy(gán 1 biến: number string boolean)
+// gán giá trị của biến đó
+
+// Biến tham chiến
+// khi copy(gán 1 biến: array object)
+// gán vị trí nhớ của biến đó
+
+
+var a = {
+    value: 1, // cấp 1: OK
+    child:{
+        valueChild :1 // cấp 2: Không thể
+    }
+}; 
+// var b = {...a}; // spread
+// var {...b} = a; // rest
+
+// deep clone
+var b = JSON.parse(JSON.stringify(a))
+// object -(JSON.stringify)> JSON -(JSON.parse)> object
+
+a.child.valueChild = 2; 
+
+// console.log(a);
+// console.log(b);
+
+// Nullish ??
+var myName="chinhpd5"; // undefined null
+// myName = myName ?? 'name default' // cách 1
+myName ??= 'name default' // cách 2
+// console.log(myName);
+
+// optional chaining ?.
+myObject = {
+    name: "chinhpd5",
+    age: 30,
+    child: {
+        name: "chinhpd6"
+    }
+}
+
+console.log(myObject.child);// undefined
+
+console.log(myObject.child?.name); // undefined
+
+// if(myObject.child){
+//     console.log(myObject.child.name); // lỗi
+//     if(myObject.child.name){
+
+//     }
+// }
+console.log(123);
+
+
+
+
+
+
+
 
 
 
