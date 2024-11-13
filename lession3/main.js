519565b2dbbad6593d3ec2a4dabbbddc1758f04b
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-11-13 08:19:51 +0700

    find, some

--- lession3/main.js
+++ lession3/main.js
@@ -61,3 +61,29 @@ const tbodyElement = document.querySelector('tbody');
 // gán giá trị trElement vào tbody thông qua phương thức 'innerHTML'
 tbodyElement.innerHTML = trElement
 
+// find: tìm kiếm 1 phần tử trong mảng thỏa mãn ĐK (return)
+// trả về phần tử đầu tiên tìm thấy và kết thúc vòng lặp
+
+const findItem = data.find((item,index)=>{
+    // console.log(index);
+    return !item.gender; // tìm kiếm phần tử có gender == false
+})
+
+// console.log(findItem);
+
+// some: trả về boolean (true | false)
+// duyệt qua 1 mảng
+// trả về true nếu có ít nhất 1 phần tử thỏa mãn ĐK (return) -> kết thúc vòng lặp
+// trả về false nếu tất các phần tử trong mảng KHÔNG thỏa mãn ĐK
+
+const checkSome = data.some((item,index)=>{
+    console.log(index);
+    return item.age > 20;
+})
+
+// Kiểm tra mảng data có 1 phần tử có điểm < 5?
+
+console.log(checkSome);
+
+
+
