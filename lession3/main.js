4c65abb7773b0028bc999c33ea600ac00a55ae44
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-11-14 12:28:13 +0700

    import bootstrap

---
+++ lession3/main.js
@@ -0,0 +1,45 @@
+// Các phương thức làm việc với mảng
+
+/**
+ * forEach
+ * find
+ * some
+ * every
+ * map
+ * filter
+ * reduce
+ */
+
+var data = [
+    {
+        name: "chinhpd5",
+        age:20,
+        gender: true,
+        mark: 10
+    },
+    {
+        name: "chinhpd6",
+        age:21,
+        gender: false,
+        mark:4
+    },
+    {
+        name: "chinhpd8",
+        age:19,
+        gender: true,
+        mark: 8
+    },
+    {
+        name: "chinhpd9",
+        age:20,
+        gender: false,
+        mark: 5
+    }
+]
+
+// forEach: duyệt qua toàn bộ các phần tử trong mảng
+
+data.forEach((item,index)=>{
+    // console.log(item); // giá trị của các phần tử trong mảng qua mỗi lần lặp
+    console.log(index); // vị trí của các phần tử, bắt đầu từ 0
+})
