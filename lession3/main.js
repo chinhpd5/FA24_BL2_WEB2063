510aa075429a65da663bedcc0f53f3009583ac7c
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-11-14 12:41:23 +0700

    forEach

--- lession3/main.js
+++ lession3/main.js
@@ -38,8 +38,28 @@ var data = [
 ]
 
 // forEach: duyệt qua toàn bộ các phần tử trong mảng
+// khai báo biến lưu trữ
+let trElements = '';
 
+// duyệt qua mảng data sử dụng forEach
 data.forEach((item,index)=>{
     // console.log(item); // giá trị của các phần tử trong mảng qua mỗi lần lặp
-    console.log(index); // vị trí của các phần tử, bắt đầu từ 0
+    // console.log(index); // vị trí của các phần tử, bắt đầu từ 0
+    // nối chuỗi += trElements
+    // sử dụng ``(tempalte string)-> ${}
+    trElements += `
+        <tr>
+            <th scope="row">${index+1}</th>
+            <td>${item.name}</td>
+            <td>${item.age}</td>
+            <td>${item.gender ? 'Nam': "Nữ"}</td>
+            <td>${item.mark}</td>
+        </tr>
+    `
 })
+
+console.log(trElements);
+// lấy ra phần tử tbody của table
+const tbodyElement = document.querySelector('tbody');
+// gán trElements vào tbody thông qua innerHTML
+tbodyElement.innerHTML = trElements;
