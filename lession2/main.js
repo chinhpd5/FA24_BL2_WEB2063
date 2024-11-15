df2455aa6c16851bdc6992482e21256e4e8a7c39
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-11-15 09:48:29 +0700

    function

---
+++ lession2/main.js
@@ -0,0 +1,31 @@
+// function
+
+// 1. Declaretion function
+function sayHello(name){
+    // logic code
+    return `Xin chào ${name}`;// template string; '' ""
+}
+
+var result = sayHello('chinhpd5');
+// console.log(result);
+
+// 2. expression function
+var sayHello1 = function (name){
+    return `Chào mừng bạn ${name}`;
+}
+
+// console.log(sayHello1('chinhpd5'));
+
+// 3. arrow function
+var sayHello2 = (name,age) => {
+    return `Bạn: ${name}, tuổi: ${age}`
+}
+
+// console.log(sayHello2("chinhpd5",20));
+
+var sayHello3 = name => `Tạm biệt ${name}`;
+console.log(sayHello3('chinhpd5'));
+
+
+
+
