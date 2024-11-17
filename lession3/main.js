80af7920769bdef6372db29c2e55de6dcaf9dbef
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-11-17 09:51:47 +0700

    import bootstrap

--- lession3/main.js
+++ lession3/main.js
@@ -11,20 +11,36 @@
  */
 
 var data = [
-    "users":[
     {
-      "id":"1",
-      "username": "chinhpd5",
-      "avatar": "https://fchat.vn/assets/img/Avatar-Facebook-trang.jpg",
-      "age": 20,
-      "note": "Mô tả 1"
+        name: "chinhpd5",
+        age:20,
+        gender: true,
+        mark: 10
     },
     {
-      "id":"2",
-      "username": "chinhpd6",
-      "avatar": "https://fchat.vn/assets/img/Avatar-Facebook-trang.jpg",
-      "age": 22,
-      "note": "Mô tả 2"
+        name: "chinhpd6",
+        age:21,
+        gender: false,
+        mark:4
+    },
+    {
+        name: "chinhpd8",
+        age:19,
+        gender: true,
+        mark: 8
+    },
+    {
+        name: "chinhpd9",
+        age:20,
+        gender: false,
+        mark: 5
     }
-  ]
-]
+];
+
+// forEach: duyệt qua toàn bộ các phần tử trong mảng
+
+data.forEach((item,index)=>{
+    // console.log(item); // giá trị các phần tử trong mảng qua mỗi lần lặp
+    // console.log(index); // vị trí các phần tử, bắt đầu 0
+    
+})
