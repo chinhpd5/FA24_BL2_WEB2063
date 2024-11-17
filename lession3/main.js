1e23b998c3ecd9df34d85635f3854930c9286136
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-11-17 07:43:17 +0700

    bootstrap

--- lession3/main.js
+++ lession3/main.js
@@ -11,5 +11,36 @@
  */
 
 var data = [
-    
+    {
+        name: "chinhpd5",
+        age:20,
+        gender: true,
+        mark: 10
+    },
+    {
+        name: "chinhpd6",
+        age:21,
+        gender: false,
+        mark:4
+    },
+    {
+        name: "chinhpd8",
+        age:19,
+        gender: true,
+        mark: 8
+    },
+    {
+        name: "chinhpd9",
+        age:20,
+        gender: false,
+        mark: 5
+    }
 ]
+
+// forEach: duyệt qua toàn bộ các phần tử trong mảng
+
+data.forEach((item,index)=>{
+    console.log(item); //giá trị các phần tử trong mảng data qua mỗi lần lặp
+    // console.log(index); // vị trí các phần tử trong mảng
+    
+})
