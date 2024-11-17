21a2f12f0717a08b5ccf7b1655edca37a52bfd25
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-11-17 09:41:34 +0700

    a

---
+++ lession3/main.js
@@ -0,0 +1,30 @@
+// các phương thức làm việc với mảng
+
+/**
+ * forEach
+ * find
+ * some
+ * every
+ * map
+ * filter
+ * reduce
+ */
+
+var data = [
+    "users":[
+    {
+      "id":"1",
+      "username": "chinhpd5",
+      "avatar": "https://fchat.vn/assets/img/Avatar-Facebook-trang.jpg",
+      "age": 20,
+      "note": "Mô tả 1"
+    },
+    {
+      "id":"2",
+      "username": "chinhpd6",
+      "avatar": "https://fchat.vn/assets/img/Avatar-Facebook-trang.jpg",
+      "age": 22,
+      "note": "Mô tả 2"
+    }
+  ]
+]
