0597859c4506417e5ba26d7b7e8970797706bfb1
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-16 16:44:16 +0700

    function

---
+++ lession2/main.js
@@ -0,0 +1,23 @@
+// Function
+
+// 1. Decleration function
+function sayHello(name) {
+  return `Xin chào ${name}`; // backtick
+}
+
+// console.log(sayHello("chinhpd5"));
+
+// 2. Expression function
+const sayHello1 = function (name) {
+  return `Chào mừng bạn ${name}`
+}
+
+// console.log(sayHello1("chinhpd5"));
+
+// 3. Arrow function
+const sayHello2 = (name, age) => {
+  return `Bạn : ${name}, tuổi: ${age}`
+}
+
+// console.log(sayHello2("chinhpd5",20));
+
